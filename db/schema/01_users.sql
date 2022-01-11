9d81e51d03449e420076a073dea5996286dc6d94
abdullahiahmed02 <aahmed_02@hotmail.com>
2022-01-10 20:25:39 -0500

    abdul first commit

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,3 +5,43 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+ name username VARCHAR (255) NOT NULL,
+ name emails VARCHAR (255) NOT NULL,
+ name phone_numbers VARCHAR (255) NOT NULL,
+ name pass VARCHAR (255) NOT NULL,
+ name is_admin BOOLEAN
+);
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name category_id ,
+  name user_id ,
+
+  name price VARCHAR (255) NOT NULL,
+  name name VARCHAR (255) NOT NULL,
+  name image VARCHAR (255) NOT NULL,
+  name create_date (timestamped?) NOT NULL,
+  name description VARCHAR(255) NOT NULL,
+  name stock VARCHAR(255) NOT NULL,
+  name is_sold BOOLEAN
+  name is_featured BOOLEAN
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name names VARCHAR (255) NOT NULL,
+
+);
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name conversation_id
+  name user_id
+  name content(body) VARCHAR(255) NOT NULL,
+
+)
+
+
