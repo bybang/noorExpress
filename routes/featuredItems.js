bcc93b7ceb4baa487dad2736435fd7456da3d5af
Mohammed Barjas <mohammedbarjas@Mohammeds-MacBook-Air.local>
2022-01-12 12:49:54 -0500

    adding try catch statement to featured items

--- routes/featuredItems.js
+++ routes/featuredItems.js
@@ -1,11 +1,16 @@
 const featuredItems = (db) => {
   return  async (req, res) => {
-    const query = {
-      text: `SELECT * FROM public.items WHERE is_featured=$1`,
-      values: [true],
-    };
-    const response = await  db.query(query);
-    res.status(200).send(response.rows);
+    try {
+      const query = {
+        text: `SELECT * FROM public.items WHERE is_featured=$1`,
+        values: [true],
+      };
+      const response = await  db.query(query);
+      res.status(200).send(response.rows);
+    } catch (e) {
+      console.error(e);
+      res.status(500).send(e);
+    }
   };
 };
 
