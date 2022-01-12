7ea3716261a7a664a6cd75ab7d47acc4971673a4
Mohammed Barjas <mohammedbarjas@Mohammeds-MacBook-Air.local>
2022-01-12 11:49:31 -0500

    filterByPrice feature

---
+++ routes/filterByPrice.js
@@ -0,0 +1,23 @@
+const filterByPrice = (db) => {
+  return async (req, res) => {
+    try {
+      const { highToLow } = req.params;
+      console.log("Filterriny by Price", highToLow, typeof highToLow);
+      let text = `SELECT * FROM public.items ORDER BY price `;
+      if (highToLow == 1) {
+        text = `${text}DESC`;
+      } else {
+        text = `${text}ASC`;
+      }
+      const query = {
+        text,
+      };
+      const response = await db.query(query);
+      res.status(200).send(response.rows);
+    } catch (error) {
+      console.error(e);
+    }
+  };
+};
+
+module.exports = filterByPrice;
