63ab410c867345a62acb38cb8e2276a6a9f60df5
Mohammed Barjas <mohammedbarjas@Mohammeds-MacBook-Air.local>
2022-01-12 19:22:40 -0500

    Mark Item Sold

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const markSold = require("./routes/markSold");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.patch('/markAsSold/:itemId', markSold(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
